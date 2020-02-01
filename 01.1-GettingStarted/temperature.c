70f136b04b876a36814f37a1ddd1d9916b1d2ecf
Adam Corbett <kehvarl@gmail.com>
2020-01-31 19:47:27 -0700

    spellcheck

--- 01.1-GettingStarted/temperature.c
+++ 01.1-GettingStarted/temperature.c
@@ -6,17 +6,17 @@
 
 int main()
 {
-    int fahr, celsius;
+    int fahrenheit, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
     upper = 300;    /* upper limit of temperature table */
     step = 20;      /* increment between entries in table */
 
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr + step;
+    fahrenheit = lower;
+    while (fahrenheit <= upper) {
+        celsius = 5 * (fahrenheit - 32) / 9;
+        printf("%d\t%d\n", fahrenheit, celsius);
+        fahrenheit = fahrenheit + step;
     }
 }
