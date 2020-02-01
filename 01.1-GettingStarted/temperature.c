acd4ffc043b35ab50b3a0a70acb420e6df6deeaa
Adam Corbett <kehvarl@gmail.com>
2020-01-31 19:47:04 -0700

    temperature conversion F to C

---
+++ 01.1-GettingStarted/temperature.c
@@ -0,0 +1,22 @@
+//
+// Created by Adam on 1/31/2020.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit of temperature table */
+    step = 20;      /* increment between entries in table */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
