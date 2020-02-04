0d7eaf031dc08591e546755390ea450e6d46eb63
Adam Corbett <kehvarl@gmail.com>
2020-02-03 19:31:12 -0700

    Create a function that uses call-by-value.

--- 01-GettingStarted/testpower.c
+++ 01-GettingStarted/testpower.c
@@ -5,12 +5,15 @@
 #include <stdio.h>
 
 int power(int base, int n);
+int cbv_power(int base, int n);
 
 int main()
 {
     int i;
-    for (i = 0; i < 10; ++i)
+    for (i = 0; i < 10; ++i) {
         printf("%d %d %d \n", i, power(2, i), power(-3, i));
+        printf("%d %d %d \n", i, cbv_power(2, i), cbv_power(-3, i));
+    }
     return 0;
 }
 
@@ -21,5 +24,16 @@ int power (int base, int n)
 
     for (i = 1; i <= n; ++i)
         p = p * base;
+    return p;
+}
+
+int cbv_power(int base, int n)
+{
+    int p;
+
+    for(p = 1; n > 0; --n)
+    {
+        p = p * base;
+    }
     return p;
 }
