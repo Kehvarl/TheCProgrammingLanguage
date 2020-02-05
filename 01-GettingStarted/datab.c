9879c0029d0dd7d64902b9678d17d4534fb103de
Adam Corbett <kehvarl@gmail.com>
2020-02-04 19:17:50 -0700

    removing extern and just using a global definition.

---
+++ 01-GettingStarted/datab.c
@@ -0,0 +1,31 @@
+//
+// Created by Adam on 2/4/2020.
+//
+
+#include <stdio.h>
+
+#define MAX_LINE  1000
+#define TAB_STOP  8
+
+int main()
+{
+    int c, i, s, spaces;
+    char text[MAX_LINE];
+    i = 0;
+    while((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            spaces = TAB_STOP - ((i % TAB_STOP));
+            printf("need %d spaces \n", spaces);
+            for (s = 0; s < spaces; ++s) {
+                text[i] = ' ';
+                ++i;
+            }
+        }
+        else
+            text[i] = (char)c;
+        ++i;
+    }
+    printf("%s", text);
+}
